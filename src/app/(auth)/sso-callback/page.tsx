ccee3d96151d4c8be96ee4725589636ff4605a89
Jaydeep Patil <jaydeeppatil3704@gmail.com>
2025-06-14 23:40:04 +0530

    changes sso-callback page

--- src/app/(auth)/sso-callback/page.tsx
+++ src/app/(auth)/sso-callback/page.tsx
@@ -1,10 +1,17 @@
 import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 
 export default function SSOCallbackPage() {
   return (
+    <div className="flex w-screen h-screen justify-center">
     <AuthenticateWithRedirectCallback
     signInForceRedirectUrl={"/boards"}
     signUpForceRedirectUrl={"/boards"}
     />
+    <div className="flex items-center justify-center flex-col gap-4s">
+       <Loader2 className="animate-spin"/>
+       <p className="text-muted-foreground">Signing In ...</p> 
+    </div>
+    </div>
   );
 }
